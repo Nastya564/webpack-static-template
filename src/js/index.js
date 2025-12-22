4d70e15c16afcf18e76f921a4da640e8af1403c6
nastya564 <nastya56499@yandex.ru>
2025-12-22 22:31:19 +0300

    one

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,19 @@
 import '../scss/style.scss'
 
-console.log('Works!')
+const btnReadMore = document.getElementById('btn-read-more')
+const infoText = document.querySelector('.main__info-text')
+const svgElement = document.getElementById('btn-read-more-svg')
+
+btnReadMore.addEventListener('click', function () {
+  const isExpanded = infoText.classList.contains('expanded')
+
+  if (isExpanded) {
+    infoText.classList.remove('expanded')
+    btnReadMore.textContent = 'Показать все'
+    svgElement.style.transform = ''
+  } else {
+    infoText.classList.add('expanded')
+    btnReadMore.textContent = 'Скрыть'
+    svgElement.style.transform = 'rotate(180deg)'
+  }
+})
